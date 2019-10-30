561a81df7aef522ba4ff587073e19030799c43bd
Sad-Boy 1922 <55779203+Sad-Boy-1922@users.noreply.github.com>
2019-10-30 16:45:34 +0700

    Add files via upload

---
+++ black.sh
@@ -0,0 +1,617 @@
+#!/system/xbin/bash
+#pake program bash shell
+#Ga usah Recode Ya
+#recode ga akan menjadikanmu sebagai programer
+#Tapi Kalo mempelajari Source codenya silahkan
+clear
+blue='\033[34;1m'
+green='\033[32;1m'                                        
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'                                           
+yellow='\033[33;1m'
+sleep 1
+echo "\033[34;1m   .............           "
+echo "\033[34;1m           ..,.:ccc,.      "                          
+echo "\033[34;1m          ......'''.lxO.   "                       
+echo "\033[34;1m .....''''..........,:ld;   "                        
+echo "\033[34;1m         .'.....:::.,,.x,       "                       
+echo "\033[34;1m      ..'''.            0Xxoc:,.  ...\\ "         
+echo "\033[34;1m  ....                ,ONkc.,.cokOdc',. \ "       
+echo "\033[34;1m .                   OMo           ':dd  . "       
+echo "\033[34;1m                   dMc              O:OO "       
+echo "\033[34;1m                  0M.                 :o. "   
+echo "\033[34;1m                    .Wd  "                         
+echo "\033[34;1m                     .XO," ==Black Dragon====
+echo "\033[34;1m                       ,d0Odlc.,.." By Sad-Boy 1922
+echo "\033[34;1m                          ..',:cdOOd::,."
+echo "\033[34;1m                                    .:d;.':."
+echo "\033[34;1m                                       'd,  .'"
+echo "\033[34;1m                                         l   .."
+echo "\033[34;1m                                          .o"
+echo "\033[34;1m                                            c"
+echo "\033[34;1m                                           .'"
+echo "\033[34;1m                                            ."
+sleep 1
+echo "\033[33;1mWelcome To Black Dragon Tools"
+sleep 1
+echo "\033[31;1mThe Ultimate Shadow Power"
+sleep 1
+echo "\\033[34;1minstalling Tools....."
+sleep 1
+echo "\033[31;1mwait...................."
+toilet -f slant -F gay "Black Dragon" | lolcat
+echo "\033[34;1m"
+cal
+date
+sleep 1
+echo "\033[31;1mwait...................."
+sleep 1
+echo "\033[31;1m                   /\  /--\  "                
+echo "\033[31;1m           _______/  \/    1 "
+echo "\033[31;1m  /\       /____            1 "
+echo "\033[31;1m  1-------/ /__|    O  /    1 "
+echo "\033[31;1m  \--------____       /    1 "
+echo "\033[31;1m   v v v v v \ \_____/  \ 1 "
+echo "\033[31;1m   /\/\/\/\/\/\ /         \ " 
+echo "\033[31;1m  \_______________      /" 
+echo "\033[34;1m   Black Dragon   \    / "
+echo "\033[34;1m   The Ultimate Shadow System "
+echo "\033[34;1m   Powered By Sad Boy 1922 "
+sleep 1
+echo "\033[31;1m____________________________________________ "
+echo "\033[32;1m||Tools : Black Dragon                       ||" 
+echo "\033[32;1m||Creator : Sad-Boy 1922                     ||"
+echo "\033[32;1m||Team : Black Coder Crush and Silent Ghost  || "
+echo "\033[32;1m||Youtube : Sad Boy 1922                     || "
+echo "\033[31;1m||___________________________________________|| "
+echo "\033[33;1m[~Chose Tools :                              ||"
+echo "\033[31;1m||___________________________________________||"
+echo "\033[33;1m||                                           ||"
+echo "\033[31;1m|| [1].Crack Facebook(Black Dragon)          ||"
+echo "\033[33;1m|| [2].Sad Boy Ddos(Black Dragon)            ||"
+echo "\033[33;1m|| [3].Admin Finder(Black Dragon)            ||"
+echo "\033[33;1m|| [4].Sql Map                               ||"
+echo "\033[31;1m|| [5].MetasPloit                            ||"
+echo "\033[31;1m|| [6].Black Hydra                           ||"
+echo "\033[33;1m|| [7].RouterSploit                          ||"
+echo "\033[33;1m|| [8].Ip Geolocation                        ||"
+echo "\033[31;1m|| [9].Kali Net Hunter(Os)                   ||"
+echo "\033[33;1m|| [10].Ubuntu(Os)                           ||"
+echo "\033[33;1m|| [11].N map                                ||"
+echo "\033[33;1m|| [12].Angry Fuzzer                         ||"
+echo "\033[31;1m|| [13].Fedora                               ||"
+echo "\033[33;1m|| [14].Cupp3                                ||"
+echo "\033[33;1m|| [15].Chat Termux                          ||"
+echo "\033[33;1m|| [16].Google Dorker                        ||"
+echo "\033[31;1m|| [17].Weeman(Phisng)                       ||"
+echo "\033[31;1m|| [18].Ngrok(Scam Page)                     ||"
+echo "\033[33;1m|| [19].Hash Buster                          ||"
+echo "\033[33;1m|| [20].Sudo                                 ||"
+echo "\033[31;1m|| [21].Air Crack                            ||"
+echo "\033[33;1m|| [22].Joomla Scanner                       ||"
+echo "\033[33;1m|| [23].bing-ip2hosts                        ||"
+echo "\033[33;1m|| [24].Blue Maho                            ||"
+echo "\033[33;1m|| [25].Blue Pot                             ||"
+echo "\033[33;1m|| [26].Honey Pot                            ||"
+echo "\033[31;1m|| [27].Auto Deface 1(Bot)                   ||"
+echo "\033[31;1m|| [28].Auto Deface 2(Bot)                   ||"
+echo "\033[33;1m|| [29].Mailer Sender                        ||"
+echo "\033[31;1m|| [30].Wordpress Bruterforce                ||"
+echo "\033[31;1m|| [31].Com Fabrik Exploiter                 ||"
+echo "\033[33;1m|| [32].Termux Color                         ||"
+echo "\033[33;1m|| [33].F Society                            ||"
+echo "\033[31;1m->Press 0 To Exit                            ||"
+echo "\033[32;1m||___________________________________________||"
+read -p "root@Sad Boy 1922]~>" sad
+
+if [ $sad = 1 ] || [ $sad = 1 ]
+then
+clear
+echo "\033[31;1m+÷÷÷÷[]===============> "
+figlet "Crack Facebook " | lolcat
+echo "\033[31;1m+÷÷÷÷[]===============> "
+pkg install git
+pkg install python
+pkg install python2
+pip2 install mechanize
+pip2 install requests
+git clone https://github.com/SadBoy1922/black
+cd black
+python2 black.py
+fi
+
+
+if [ $sad = 2 ] || [ $sad = 2 ]
+then
+clear
+echo "\033[31;1m+÷÷÷÷[]===============> "
+figlet "Ddos" | lolcat
+echo "\033[31;1m+÷÷÷÷[]===============> "
+pkg update && pkg upgrade
+pkg install python2
+pkg install figlet
+pkg install git
+git clone https://github.com/SadBoy1922/ddos
+cd ddos
+python2 ddos.py
+fi
+
+if [ $sad = 3 ] || [ $sad = 3 ]
+then
+echo "\033[31;1m+÷÷÷÷[]===============> "
+figlet "Admin Finder" | lolcat
+echo "\033[31;1m+÷÷÷÷[]===============> "
+pkg update && pkg upgrade
+pkg install git
+pkg install python2
+pip2 install mechanize
+git clone https://github.com/SadBoy1922/PageFind
+cd PageFind
+python2 adfin.py 
+fi
+
+if [ $sad = 4 ] || [ $sad = 4 ]
+then
+clear
+echo "\033[31;1m+÷÷÷÷[]===============> "
+figlet "Sql Map" | lolcat
+echo "\033[31;1m+÷÷÷÷[]===============> "
+apt update && apt upgrade
+apt install python2
+apt install git
+https://github.com/sqlmapproject/sqlmap
+cd sqlmap
+python2 sqlmap.py
+fi
+
+if [ $sad = 5 ] || [ $sad = 5 ]
+then
+clear
+echo "\033[31;1m+÷÷÷÷[]===============> "
+figlet "Metasploit" | lolcat
+echo "\033[31;1m+÷÷÷÷[]===============> "
+apt update && apt upgrade
+apt install git
+git clone https://github.com/verluchie/termux-metasploit
+cd termux-metasploit
+chmod 777 install.sh
+sh install.sh
+fi
+
+if [ $sad = 6 ] || [ $sad = 6 ]
+then
+clear
+echo "\033[31;1m+÷÷÷÷[]===============> "
+figlet "Black Hydra" | lolcat
+echo "\033[31;1m+÷÷÷÷[]===============> "
+apt update && apt upgrade
+apt install git hydra
+git clone https://github.com/Gameye98/Black-Hydra/
+cd b-hydra
+mv blackhydra.py main.py
+chmod +x main.py
+ls
+python2 main.py
+fi
+
+if [ $sad = 7 ] || [ $sad = 7 ]
+then
+clear
+echo "\033[31;1m+÷÷÷÷[]===============> "
+figlet "RouterSploit" | lolcat
+echo "\033[31;1m+÷÷÷÷[]===============> "
+pkg update -y
+pkg install -y git
+pkg install -y python2
+cd /data/data/com.termux/files/home && git clone https://github.com/reverse-shell/routersploit.git
+cd /data/data/com.termux/files/home && cd routersploit
+pip2 install -r requirements.txt
+pip2 install -r requirements-dev.txt
+pip2 install -r requests
+python2 rsf.py
+fi
+
+
+if [ $sad = 8 ] || [ $sad = 8 ]
+then
+clear
+echo "\033[31;1m+÷÷÷÷[]===============> "
+figlet "IP GeoLocation" | lolcat
+echo "\033[31;1m+÷÷÷÷[]===============> "
+apt update && apt upgrade
+apt install git
+apt install python
+git clone https://github.com/maldevel/IPGeolocation/
+cd ipgeolocation
+mv ipgeolocation.py main.py
+chmod +x main.py
+pip install -r requirements.txt
+./main.py -h
+fi
+
+if [ $sad = 9 ] || [ $sad = 9 ]
+then
+clear
+echo "\033[31;1m+÷÷÷÷[]===============> "
+figlet "Kali Nethunter" | lolcat
+echo "\033[31;1m+÷÷÷÷[]===============> "
+apt update && apt upgrade
+git clone https://github.com/Hax4us/Nethunter-In-Termux
+cd b-hydra
+cd nethunter
+cp kalinethunter ~
+chmod +x kalinethunter
+./kalinethunter
+fi
+
+if [ $sad = 10 ] || [ $sad = 10 ]
+then
+clear
+echo "\033[31;1m+÷÷÷÷[]===============> "
+figlet " Ubuntu " | lolcat
+echo "\033[31;1m+÷÷÷÷[]===============> "
+apt install git
+apt install wget
+apt install proot
+git clone https://github.com/Neo-Oli/termux-ubuntu.git
+cd termux-ubuntu
+chmod +x ubuntu.sh
+./ubuntu.sh
+./startubuntu.sh
+fi
+
+if [ $sad = 11 ] || [ $sad = 11 ]
+then
+clear
+echo "\033[31;1m+÷÷÷÷[]===============> "
+figlet " Nmap " | lolcat
+echo "\033[31;1m+÷÷÷÷[]===============> "
+pkg update && apt upgrade
+pkg install nmap
+fi
+
+if [ $sad = 12 ] || [ $sad = 12 ]
+then
+clear
+echo "\033[31;1m+÷÷÷÷[]===============> "
+figlet " Angry Fuzzer " | lolcat
+echo "\033[31;1m+÷÷÷÷[]===============> "
+pkg update -y
+pkg install -y git
+pkg install -y python2
+cd /data/data/com.termux/files/home && git clone https://github.com/ihebski/angryFuzzer.git
+cd /data/data/com.termux/files/home && cd angryFuzzer
+cd /data/data/com.termux/files/home && pip2 install -r requirements.txt
+cd /data/data/com.termux/files/home && pip2 install requests
+cd /data/data/com.termux/files/home
+python2 angryFuzzer.py
+fi
+
+if [ $sad = 13 ] || [ $sad = 13 ]
+then
+clear
+echo "\033[31;1m+÷÷÷÷[]===============> "
+figlet " Fedora " | lolcat
+echo "\033[31;1m+÷÷÷÷[]===============> "
+pkg update -y
+pkg install -y git
+pkg install -y wget
+apt update && apt install wget -y && /data/data/com.termux/files/usr/bin/wget https://raw.githubusercontent.com/nmilosev/termux-fedora/master/termux-fedora.sh
+sh termux-fedora.sh f26_arm
+fi
+
+if [ $sad = 14 ] || [ $sad = 14 ]
+then
+clear
+echo "\033[31;1m+÷÷÷÷[]===============> "
+figlet " Cupp3 " | lolcat
+echo "\033[31;1m+÷÷÷÷[]===============> "
+pkg update -y
+pkg install -y git
+pkg install -y python
+cd /data/data/com.termux/files/home && git clone https://github.com/Mebus/cupp.git
+python cupp3.py
+fi
+
+if [ $sad = 15 ] || [ $sad = 15 ]
+then
+clear
+echo "\033[31;1m+÷÷÷÷[]===============> "
+figlet " Chatan Termux " | lolcat
+echo "\033[31;1m+÷÷÷÷[]===============> "
+pkg install irssi
+irssi
+fi
+
+if [ $sad = 16 ] || [ $sad = 16 ]
+then
+clear
+echo "\033[31;1m+÷÷÷÷[]===============> "
+figlet " Google Dorker " | lolcat
+echo "\033[31;1m+÷÷÷÷[]===============> "
+apt update 
+apt upgrade
+apt install python2
+apt install git
+git clone https://github.com/XG77Z10/Google-Dork
+mv Google-Dork $HOME
+cd $HOME/Google-Dork
+python2 Google.py
+fi
+
+if [ $sad = 17 ] || [ $sad = 17 ]
+then
+clear
+echo "\033[31;1m+÷÷÷÷[]===============> "
+figlet " Weeman(Phising) " | lolcat
+echo "\033[31;1m+÷÷÷÷[]===============> "
+apt update && upgrade
+apt install python2
+apt install git
+git clone https://github.com/evait-security/weeman
+cd weeman
+chmod +x weeman.py
+python2 weeman.py
+fi
+
+if [ $sad = 18 ] || [ $sad = 18 ]
+then
+clear
+echo "\033[31;1m+÷÷÷÷[]===============> "
+figlet " Ngrok(Scam page) " | lolcat
+echo "\033[31;1m+÷÷÷÷[]===============> "
+apt install wget
+mkdir ngrok
+cd ~/ngrok
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip
+unzip ngrok-stable-linux-arm.zip
+cd ~/
+fi
+
+if [ $sad = 19 ] || [ $sad = 19 ]
+then
+clear
+echo "\033[31;1m+÷÷÷÷[]===============> "
+figlet " Hash Buster " | lolcat
+echo "\033[31;1m+÷÷÷÷[]===============> "
+echo -e $green" Installing Ngrok "
+apt update && apt upgrade
+apt install python2 && apt install git
+git clone https://github.com/UltimateHackers/Hash-Buster
+cd Hash-Buster
+fi
+
+if [ $sad = 20 ] || [ $sad = 20 ]
+then
+clear
+echo "\033[31;1m+÷÷÷÷[]===============> "
+figlet " Sudo " | lolcat
+echo "\033[31;1m+÷÷÷÷[]===============> "
+apt update && apt upgrade
+pkg install git ncurses-utils
+git clone https://github.com/st42/termux-sudo.git
+cd termux-sudo
+cat sudo > /data/data/com.termux/files/usr/bin/sudo
+chmod 700 /data/data/com.termux/files/usr/bin/sudo
+fi
+
+if [ $sad = 21 ] || [ $sad = 21 ]
+then
+clear
+echo "\033[31;1m+÷÷÷÷[]===============> "
+figlet " Air Crack " | lolcat
+echo "\033[31;1m+÷÷÷÷[]===============> "
+sleep 1
+apt-get update && apt-get upgrade
+apt-get install aircrack-ng
+fi
+
+if [ $sad = 22 ] || [ $sad = 22 ]
+then
+clear
+echo "\033[31;1m+÷÷÷÷[]===============> "
+figlet " Joomla Scanner " | lolcat
+echo "\033[31;1m+÷÷÷÷[]===============> "
+sleep 1
+apt-get update && apt-get upgrade
+apt-get install git
+apt-get install perl
+git clone https://github.com/rezasp/joomscan.git
+fi
+
+if [ $sad = 23 ] || [ $sad = 23 ]
+then
+clear
+echo "\033[31;1m+÷÷÷÷[]===============> "
+figlet " bing-ip2hosts " | lolcat
+echo "\033[31;1m+÷÷÷÷[]===============> "
+sleep 1
+apt-get update && apt-get upgrade
+apt-get install wget
+wget http://www.morningstarsecurity.com/downloads/bing-ip2hosts-0.4.tar.gz && tar -xzvf bing-ip2hosts-0.4.tar.gz && cp bing-ip2hosts-0.4/bing-ip2hosts /usr/local/bin/t
+fi
+
+if [ $sad = 24 ] || [ $sad = 24 ]
+then
+clear
+echo "\033[31;1m+÷÷÷÷[]===============> "
+figlet " Blue Maho " | lolcat
+echo "\033[31;1m+÷÷÷÷[]===============> "
+sleep 1
+apt-get update && apt-get upgrade
+apt-get install git
+git clone git://git.kali.org/packages/bluemaho.git
+fi
+
+if [ $sad = 25 ] || [ $sad = 25 ]
+then
+clear
+echo "\033[31;1m+÷÷÷÷[]===============> "
+figlet " bluepot " | lolcat
+echo "\033[31;1m+÷÷÷÷[]===============> "
+sleep 1
+apt-get update && apt-get upgrade
+apt-get install git
+git clone git://git.kali.org/packages/bluepot.git
+fi
+
+if [ $sad = 26 ] || [ $sad = 26 ]
+then
+clear
+echo "\033[31;1m+÷÷÷÷[]===============> "
+figlet " honeypot " | lolcat
+echo "\033[31;1m+÷÷÷÷[]===============> "
+sleep 1
+apt-get update && apt-get upgrade
+apt-get install git && apt-get install php
+git clone https://github.com/whackashoe/php-spam-mail-honeypot.git
+fi
+
+if [ $sad = 27 ] || [ $sad = 27 ]
+then
+clear
+echo "\033[31;1m+÷÷÷÷[]===============> "
+figlet " Auto Deface " | lolcat
+echo "\033[31;1m+÷÷÷÷[]===============> "
+sleep 1
+apt-get update && apt-get upgrade
+apt-get install git 
+apt-get install wget 
+apt-get install perl 
+apt-get install unzip
+git clone https://github.com/mrcakil/bot.git
+cd bot
+unzip bot.zip
+cd xploit
+chmod 777 bot.pl
+echo -e $red" Lokasi bot ? /bot/xploit/bot.pl"
+fi
+
+if [ $sad = 28 ] || [ $sad = 28 ]
+then
+clear
+echo "\033[31;1m+÷÷÷÷[]===============> "
+figlet " Auto Deface 2 " | lolcat
+echo "\033[31;1m+÷÷÷÷[]===============> "
+sleep 1
+apt-get update && apt-get upgrade
+apt-get install git && apt-get install perl
+git clone https://github.com/Moham3dRiahi/XAttacker
+cd XAttacker
+chmod 777 XAttacker.pl
+fi
+
+if [ $sad = 29 ] || [ $sad = 29 ]
+then
+clear
+echo "\033[31;1m+÷÷÷÷[]===============> "
+figlet " Mailer Sender " | lolcat
+echo "\033[31;1m+÷÷÷÷[]===============> "
+sleep 1
+apt-get update && apt-get upgrade
+apt-get install php5-cli
+curl -sS https://getcomposer.org/installer | php
+chmod +x composer.phar
+sudo mv composer.phar /usr/bin/composer
+git clone https://github.com/pedro-stanaka/mailer-cli.git
+echo -e $red" note !! "
+echo -e $red" usage php sendmail.php notification:mailer <email> <subject> <body>; "
+echo -e $red" or "
+echo -e $red" php sendmail.php --help "
+fi
+
+if [ $sad = 30 ] || [ $sad = 30 ]
+then
+clear
+echo "\033[31;1m+÷÷÷÷[]===============> "
+figlet " Wordpress Bruterforce " | lolcat
+echo "\033[31;1m+÷÷÷÷[]===============> "
+sleep 1
+apt-get update && apt-get upgrade
+apt-get install python2
+pip install request
+git clone https://github.com/atarantini/wpbf
+fi
+
+
+if [ $sad = 31 ] || [ $sad = 31 ]
+then
+clear
+echo "\033[31;1m+÷÷÷÷[]===============> "
+figlet " Com.Fabrik Exploiter " | lolcat
+echo "\033[31;1m+÷÷÷÷[]===============> "
+sleep 1
+apt-get update && apt-get upgrade
+apt-get install wget
+apt-get install php
+wget https://pastebin.com/raw/LDvFvtUD -O com_fabrik.php
+sleep 1
+fi
+
+
+
+if [ $sad = 32 ] || [ $sad = 32 ]
+then
+clear
+echo "\033[31;1m+÷÷÷÷[]===============> "
+figlet " Termux Color " | lolcat
+echo "\033[31;1m+÷÷÷÷[]===============> "
+sleep 1
+apt-get update && apt-get upgrade
+sh -c "$(curl -fsSL https://github.com/Cabbagec/termux-ohmyzsh/raw/master/install.sh)"
+~/.termux/colors.sh
+fi
+
+
+if [ $sad = 33 ] || [ $sad = 33 ]
+then
+clear
+echo "\033[31;1m+÷÷÷÷[]===============> "
+figlet " F society " | lolcat
+echo "\033[31;1m+÷÷÷÷[]===============> "
+sleep 1
+apt-get update && apt-get upgrade
+pkg install python
+apt-get install git
+git clone https://github.com/Manisso/fsociety
+cd fsociety
+echo -e $red" python fsociety.py "
+fi
+
+if [ $sad = 0 ] || [ $sad = 0 ]
+then
+echo "\033[34;1m   .............           "
+echo "\033[34;1m           ..,.:ccc,.      "                          
+echo "\033[34;1m          ......'''.lxO.   "                       
+echo "\033[34;1m .....''''..........,:ld;   "                        
+echo "\033[34;1m         .'.....:::.,,.x,       "                       
+echo "\033[34;1m      ..'''.            0Xxoc:,.  ...\\ "         
+echo "\033[34;1m  ....                ,ONkc.,.cokOdc',. \ "       
+echo "\033[34;1m .                   OMo           ':dd  . "       
+echo "\033[34;1m                   dMc              O:OO "       
+echo "\033[34;1m                  0M.                 :o. "   
+echo "\033[34;1m                    .Wd  "                         
+echo "\033[34;1m                     .XO," ==Black Dragon====
+echo "\033[34;1m                       ,d0Odlc.,.." ====By Sad-Boy 1922===
+echo "\033[34;1m                          ..',:cdOOd::,."
+echo "\033[34;1m                                    .:d;.':."
+echo "\033[34;1m                                       'd,  .'"
+echo "\033[34;1m                                         l   .."
+echo "\033[34;1m                                          .o"
+echo "\033[34;1m                                            c"
+echo "\033[34;1m                                           .'"
+echo "\033[34;1m                                            ."
+sleep 1
+echo "\033[31;1m+÷÷÷÷[]===============> "
+toilet -f slant -F gay "Black Dragon" | lolcat
+echo "\033[31;1m+÷÷÷÷[]===============> "
+echo "\033[33;1-Thanks Cause Use Black Dragon Tools"
+sleep 1
+echo "\033[33;1-Please Subscribe Channel Youtube Sad Boy 1922"
+sleep 1
+echo "\033[31;1mExit........"
+sleep 1
+exit
+fi
